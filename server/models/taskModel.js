ffc3dd7830926437433c7e08a080d61a0bb96dc9
kchandler44 <kellychandler624@gmail.com>
2024-08-21 15:44:52 -0600

    setting up schema for gallery

--- server/models/taskModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const taskSchema = new Schema({
-    task_name: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Task', taskSchema);
